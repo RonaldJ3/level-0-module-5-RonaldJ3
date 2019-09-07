e929522a577810e3b4a036a56be2886fe7e64ae7
League <league@Groovy.local>
2019-09-07 11:15:58 -0700

    I sarted moudle 5 and prime or not

---
+++ src/PrimeOrNot.java
@@ -0,0 +1,10 @@
+import javax.swing.JOptionPane;
+
+public class PrimeOrNot {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+JOptionPane.showInputDialog(null,"Give me a number?");
+int mod = 5%2;
+	}
+
+}
