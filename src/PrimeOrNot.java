1cdb3172060f7dc437e14dedd217270400ec617f
DemoliciousDt <league@Thes-Mac-mini.local>
2019-09-14 11:26:42 -0700

    I finshed prime or not

--- src/PrimeOrNot.java
+++ src/PrimeOrNot.java
@@ -3,8 +3,16 @@ import javax.swing.JOptionPane;
 public class PrimeOrNot {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-JOptionPane.showInputDialog(null,"Give me a number?");
-int mod = 5%2;
-	}
+		String val = JOptionPane.showInputDialog(null, "Give me a number.");
+		int doom = Integer.parseInt(val);
+		for (int i = 2; i <= doom - 1; i++) {
+			if (doom % i == 0) {
+				JOptionPane.showMessageDialog(null, "Sorry not prime.");
+				System.exit(0);
+			}
+		}
 
+		JOptionPane.showMessageDialog(null, "It is prime.");
+
+	}
 }
